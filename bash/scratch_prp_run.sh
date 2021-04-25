dd4afaee8ec5aef86cc03d9eca2f5a679495c59d
charlotte-ngs <ngs.charlotte@gmail.com>
2021-04-25 16:14:35 +0200

    Added scratch run command

---
+++ bash/scratch_prp_run.sh
@@ -0,0 +1,6 @@
+
+sudo singularity shell --shell /bin/bash instance://gnmsicnt
+
+/home/gnmzws/simg/genmon-sidef/bash/run_poprep.sh -p /home/gnmzws/simg/genmon-sidef/par/gnm_config.par -d /home/gnmzws/gnm/prpinput/test_pedigree.dat.txt -Z &> `date +"%Y%m%d%H%M%S"`_prp.log
+
+
