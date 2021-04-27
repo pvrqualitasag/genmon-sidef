55fe051ee1e998c037acf4f1b1e7d6d046fbe372
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2021-04-27 17:55:18 +0200

    Apdated documentation

--- bash/scratch_prp_run.sh
+++ bash/scratch_prp_run.sh
@@ -1,8 +1,14 @@
 
 sudo singularity shell --shell /bin/bash instance://gnmsicnt
 
+# test
 /home/gnmzws/simg/genmon-sidef/bash/run_poprep.sh \
 -p /home/gnmzws/simg/genmon-sidef/par/gnm_config.par \
 -d /home/gnmzws/gnm/prpinput/test_pedigree.dat.txt -Z &> /home/gnmzws/gnm/prplog/`date +"%Y%m%d%H%M%S"`_prp.log
+
+# BV
+/home/gnmzws/simg/genmon-sidef/bash/run_poprep.sh \
+-p /home/gnmzws/simg/genmon-sidef/par/gnm_config.par \
+-d /home/gnmzws/gnm/prpinput/PopReport_Braunvieh_20210325.csv_adaptfin7.csv -Z &> /home/gnmzws/gnm/prplog/`date +"%Y%m%d%H%M%S"`_prp.log
 
 
